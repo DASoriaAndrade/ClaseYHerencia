fbaae1ff42c6a2c734c0936c103b1615ec0651cc
Daniel <daniel.soria@sistemas.edu.bo>
2023-03-24 01:33:27 -0400

    añadiendo la clase Interesado al proyecto

---
+++ ClasesYHerencia/Estudiante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesHerencia
+{
+    internal class Estudiante : Usuario
+    {
+        /// <summary>
+        /// Este es un constructror vacio
+        /// </summary>
+        public Estudiante() { }
+
+        public string Matricula { get; set; }
+    }
+}
